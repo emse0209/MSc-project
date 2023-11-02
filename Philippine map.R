033b0616745f8fe4740b3d7342d2aac4767d0415
Emma Jarlbaek <emma@jarlbaek.dk>
2023-11-02 13:43:54 +0100

    luzon map

--- Philippine map.R
+++ Philippine map.R
@@ -14,15 +14,40 @@ head(w2hr)
 tail(w2hr)
 
 #extract Philippines data
-
 Philippines<-w2hr %>%
   filter(region =='Philippines')
 head(Philippines)
 tail(Philippines)
 
-#mapping Philippines data
-map<-map_data('Philippines')
-ggplot()+
-  geom_polygon(data = Philippines, aes(x= long, y= lat, group=group))+
-  coord_quickmap()
-map_data(Philippines)
+#map Philippines data
+map<-ggplot()+
+  geom_polygon(data = Philippines, aes(x= long, y= lat, group=group), colour='black', fill='light grey')+
+  xlab('Longitude')+
+  ylab('Latitude')+
+  coord_quickmap()+ #makes sure aspect ratios remains unchanged i.e. size of map will not change when exporting
+  theme_bw
+map
+
+#extract Luzon from Philppines
+Luzon<-Philippines %>%
+  filter(subregion =='Luzon')
+head(Luzon)
+tail(Luzon)
+
+map_luzon<-ggplot()+
+  geom_polygon(data=Luzon, aes(x= long, y= lat, group=group), colour='black', fill='light grey')+
+  xlab('Longitude')+
+  ylab('Latitude')+
+  geom_point(data = labs, aes(x= long, y=lat), shape=21, color='blue', fill='blue', size=5) +
+  geom_text(data = labs, aes(x = long, y = lat, label = names), hjust=0, nudge_x = 1)+
+  coord_quickmap()+
+  theme_bw()
+
+map_luzon
+
+
+#Adding locations of sites
+labs<- tibble(
+  long = c(121.384721, 121.060000),
+  lat =  c(16.645834, 18.240000),
+  names = c("Lower Magat Ecotourism Park", "Calanasan River"))
